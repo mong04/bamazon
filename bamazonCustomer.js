731854e15c9daf8ef3539a52471dd407776d0532
Andrew Sanchez <bballbizniz@aol.com>
2017-07-08 18:06:18 -0700

    working code with dynamic tables

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
+const Table = require('cli-table');
 
 var connection = mysql.createConnection({
     host     : 'localhost',
@@ -14,14 +15,65 @@ var connection = mysql.createConnection({
       return;
     }
    displayItems();
-    // console.log('connected as id ' + connection.threadId);
   });
 
   function displayItems(){
       connection.query('SELECT * FROM products', function(error, res, fields){
-          for (i = 0; i < res.length; i++){
-              let item = res[i];
-              console.log('\nID: ' + item.item_id + ' Name: ' + item.product_name + ' Price: $' + item.price);
+        var table = new Table({
+            head: ['Item ID', 'Product', 'Department', 'Price', 'In Stock'], 
+            colWidths: [20, 30, 20, 20, 20]
+        });
+        for (let value of res){
+            table.push(
+                [value.item_id, value.product_name, value.department_name, value.price, value.stock_quantity]
+                
+            );
+        }
+        console.log(table.toString());
+        takeOrder();
+        })
+  }
+
+  const priceCalculate = (price, quantity) => console.log(`Order successful, your total is $${price * quantity}`);
+
+  function takeOrder(){
+      inquirer.prompt([
+          {
+              type: 'input',
+              message: "Please enter the ID of the product you'd like to buy.",
+              name: 'productID',
+              validate: function(value){
+                  if (value.length){
+                      return true;
+                  }
+                  console.log('Please enter a product ID.')
+              }
+          },
+          {
+              type: 'input',
+              message: "Please enter the quantity of the product you'd like to buy.",
+              name: 'itemQuantity'
           }
-      })
+        ]).then(function(response){
+            var query = "SELECT * FROM products WHERE item_id = ?";
+            connection.query(query, [response.productID], function(err, res){
+                if (res[0].stock_quantity < response.itemQuantity){
+                    console.log('Insufficient quantity!');
+                    // displayItems();
+                    setTimeout(displayItems, 1500);
+                }
+                else{
+                    let query = "UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id = ?";
+                    connection.query(query, [response.itemQuantity, response.productID], function(err, res, fields){
+                        if (err) throw err;
+                            let queryPrice = "SELECT price FROM products WHERE item_id = ?";
+                            connection.query(queryPrice, [response.productID], function(err, res, fields){
+                                priceCalculate(res[0].price, response.itemQuantity);
+                                // displayItems();
+                                setTimeout(displayItems, 1500);
+                            })
+                    })
+                }
+            })
+        })
   }
