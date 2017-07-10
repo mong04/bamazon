6982749820fa83a38cf0b352199f8d24e7986f18
Andrew Sanchez <bballbizniz@aol.com>
2017-07-10 14:25:59 -0700

    comments added to make code more readable

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,13 +2,15 @@ const mysql = require('mysql');
 const inquirer = require('inquirer');
 const Table = require('cli-table');
 
+//establish connection to MySQL
 var connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
     password : 'Fightingpadre!$',
     database : 'bamazonDB'
   });
-   
+
+  //check if connection had errors, if not perform app function 
   connection.connect(function(err) {
     if (err) {
       console.error('error connecting: ' + err.stack);
@@ -17,6 +19,7 @@ var connection = mysql.createConnection({
    displayItems();
   });
 
+  //Call on 'cli-table' to create new table, push MySQL query into the table then display table
   function displayItems(){
       connection.query('SELECT * FROM products', function(error, res, fields){
         var table = new Table({
@@ -34,8 +37,10 @@ var connection = mysql.createConnection({
         })
   }
 
+  //calculate final price to the customer using price from MySQL query and quantity the customer provided
   const priceCalculate = (price, quantity) => console.log(`Order successful, your total is $${price * quantity}`);
 
+  //prompt the customer for the item they'd like to purchase and how many of that item.
   function takeOrder(){
       inquirer.prompt([
           {
@@ -55,20 +60,23 @@ var connection = mysql.createConnection({
               name: 'itemQuantity'
           }
         ]).then(function(response){
+            //take customer input to query MySQL for items matching item_id
             var query = "SELECT * FROM products WHERE item_id = ?";
             connection.query(query, [response.productID], function(err, res){
+                //check if there is enough item in stock to fulfill order
                 if (res[0].stock_quantity < response.itemQuantity){
                     console.log('Insufficient quantity!');
                     setTimeout(displayItems, 1500);
                 }
                 else{
+                    //update stock_quantity to reflect items sold
                     let query = "UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id = ?";
                     connection.query(query, [response.itemQuantity, response.productID], function(err, res, fields){
                         if (err) throw err;
                             let queryPrice = "SELECT price FROM products WHERE item_id = ?";
                             connection.query(queryPrice, [response.productID], function(err, res, fields){
+                                //call on priceCalculate function to take MySQL price response and customer quantity input to calculate final cost
                                 priceCalculate(res[0].price, response.itemQuantity);
-                                // displayItems();
                                 setTimeout(displayItems, 1500);
                             })
                     })
--- bamazonManager.js
+++ bamazonManager.js
@@ -2,6 +2,7 @@ const inquirer = require('inquirer');
 const mysql = require('mysql');
 const Table = require('cli-table');
 
+//establish connection to mySQL
 var connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
@@ -9,6 +10,7 @@ var connection = mysql.createConnection({
     database : 'bamazonDB'
   });
    
+  //check to make sure connection successful then perform app function if connection established
   connection.connect(function(err) {
     if (err) {
       console.error('error connecting: ' + err.stack);
@@ -17,6 +19,7 @@ var connection = mysql.createConnection({
     mainMenu();
 });
 
+//display items currently for sale on the database.
 const displayItems = () => {
     connection.query('SELECT * FROM products', function(err, res, fields){
         if (err) throw err;
@@ -34,6 +37,7 @@ const displayItems = () => {
     })
 }
 
+//query MySQL for all items with less than 5 items in stock then display items in a new table
 const lowInventory = () => {
     connection.query('SELECT * FROM products WHERE stock_quantity < 5', function(err, res, fields){
         if (err) throw err;
@@ -51,6 +55,7 @@ const lowInventory = () => {
     })
 }
 
+//query MySQL to update the stock of an item of the manager's choosing
 const addInventory = (item, quantity) => {
     let query = "UPDATE products SET stock_quantity = stock_quantity + ? WHERE item_id = ?";
     connection.query(query, [quantity, item], function(err, res, fields){
@@ -60,6 +65,7 @@ const addInventory = (item, quantity) => {
     })    
 }
 
+//question object for asking what item and how much of it to stock. To be used in inquirer.prompt
 let inventoryQuestions = [
     {
         type: 'input',
@@ -79,6 +85,7 @@ let inventoryQuestions = [
     }    
 ];
 
+//query MySQL to add a new product to the database to be sold
 const addProduct = (name, department, price, quantity) => {
     let query = "INSERT INTO products(product_name, department_name, price, stock_quantity) VALUES (?,?,?,?)";
     connection.query(query, [name, department, price, quantity], function(err, res, fields){
@@ -88,6 +95,7 @@ const addProduct = (name, department, price, quantity) => {
     })
 }
 
+//question object get product details for new product to be added. To be used in inquirer.prompt
 let productQuestions = [
     {
         type: 'input',
@@ -130,6 +138,7 @@ let productQuestions = [
     }
 ]
 
+//main app function provides UI/UX, prompts user to see what they want to do
 const mainMenu = () => {
     inquirer.prompt([
         {
@@ -148,18 +157,24 @@ const mainMenu = () => {
             lowInventory();
             break;
         case "Add To Inventory":
+            //prompt user with quesitons from previously declared inventoryQuestions
             inquirer.prompt(inventoryQuestions).then(function(choices){
                 let item = choices.productID;
                 let quantity = choices.itemQuantity;
+
+                //pass responses into function
                 addInventory(item, quantity);
             })
             break;
         case "Add New Product":
+        //prompt user with quesitons from previously declared productQuestions
             inquirer.prompt(productQuestions).then(function(product){
                 let name = product.name;
                 let department = product.department;
                 let price = product.price;
                 let quantity = product.quantity
+
+                //pass productQuestions responses into function
                 addProduct(name, department, price, quantity);
             })
             break;
